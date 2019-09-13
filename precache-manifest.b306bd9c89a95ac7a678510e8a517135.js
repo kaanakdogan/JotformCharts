d3114547415f3d212909a04b81d9d2f5930d0325
Kaan Akdogan <kaan.akdogann@gmail.com>
2019-09-13 13:09:45 +0300

    Updates

---
+++ precache-manifest.b306bd9c89a95ac7a678510e8a517135.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ff642fa4d59764cb36a143ce404272c",
+    "url": "/JotformCharts/index.html"
+  },
+  {
+    "revision": "9e11d17c67bb6ec41782",
+    "url": "/JotformCharts/static/js/2.d5414a27.chunk.js"
+  },
+  {
+    "revision": "db77b476775d65417e80",
+    "url": "/JotformCharts/static/js/main.8168ed21.chunk.js"
+  },
+  {
+    "revision": "dd01527e72825e592e9d",
+    "url": "/JotformCharts/static/js/runtime~main.585dac56.js"
+  },
+  {
+    "revision": "85846383a055bdbe42e87ecf1fd6f875",
+    "url": "/JotformCharts/static/media/delete.85846383.svg"
+  },
+  {
+    "revision": "c8cf4890d9ed542fa9e03cb797c55039",
+    "url": "/JotformCharts/static/media/logo.c8cf4890.svg"
+  },
+  {
+    "revision": "4ec93397b27693c8e00c6f38d8b01eb7",
+    "url": "/JotformCharts/static/media/opt.4ec93397.svg"
+  }
+]);
